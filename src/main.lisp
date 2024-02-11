62f4328e015d5a9a98c8bce6715ada642d26bbc2
Marcos Magueta <maguetamarcos@gmail.com>
2024-02-10 21:32:10 -0300

    add variable substitution

--- src/main.lisp
+++ src/main.lisp
@@ -95,6 +95,9 @@
 	 (arguments (arguments obj))
 	 (initial-term (cond
 			 ((string= func-name "+") "(i32.add")
+			 ((string= func-name "-") "(i32.sub")
+			 ((string= func-name "*") "(i32.mul")
+			 ((string= func-name "/") "(i32.div")
 			 (t (format nil "(call $~a" func-name)))))
     (map 'list #'emit arguments)
     (setf (context obj)
@@ -104,15 +107,32 @@
 					   ")")
 			 ,initial-term)))))
 
+(defclass substitution ()
+  ((label
+    :initarg :label
+    :accessor label)
+   (global?
+    :initarg :global?
+    :accessor global?)
+   (context
+    :initarg :context
+    :accessor context)))
+
+(defmethod emit ((obj substitution))
+  (if (global? obj)
+      (setf (context obj)
+	    (format nil "(global.get $~a)" (label obj)))
+      (setf (context obj)
+	    (format nil "(local.get $~a)" (label obj)))))
+
 (defun chimelisp/compile ()
   (let* ((context "")
 	 (literal
 	   (make-instance 'literal :value 123 :context context))
-	 (abstraction-1 (make-instance 'abstraction :context "" :body literal :arguments '("x" "y") :name "xyz" :return-type "i32"))
+	 (abstraction-1 (make-instance 'abstraction :context "" :body (make-instance 'substitution :label "x" :global? nil :context "") :arguments '("x") :name "xyz" :return-type "i32"))
 	 (application (make-instance 'application :context ""
 						  :func abstraction-1
-						  :arguments (list (make-instance 'literal :value 1 :context "")
-								   (make-instance 'literal :value 2 :context ""))))
+						  :arguments (list (make-instance 'literal :value 1 :context ""))))
 	 (abstraction
 	   (make-instance 'abstraction :name "abc" :context context :body application :arguments '() :return-type "i32"))
 	 (_ (emit abstraction-1))
@@ -122,7 +142,10 @@
     (if (uiop:file-exists-p file-name)
 	(delete-file file-name))
     (with-open-file (stream file-name :direction :output)
-      (format stream "(module (import \"env\" \"print_string\" (func $print_string (param i32))) (import \"env\" \"buffer\" (memory 1)) ~a ~a)" (context abstraction-1) compiled-context))
+      (format stream
+	      "(module (import \"env\" \"print_string\" (func $print_string (param i32))) (import \"env\" \"buffer\" (memory 1)) ~a ~a)"
+	      (context abstraction-1)
+	      compiled-context))
     ;; (uiop:run-program (list "wat2wasm" file-name "-o" "./WASM/test.wasm")
 		      ;; :error-output :string
 		      ;; :ignore-error-status nil)))
